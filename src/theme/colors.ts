b869e2a930fe50810dab96df906928b959c57534
morshedx <morshedx@users.noreply.github.com>
2022-01-10 23:25:55 +0600

    First Commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  transparent: "transparent", // 0
+  black: "#292929", // 1
+  white: "#ffffff", // 2
+  textColor: "#292929", // 3
+  lightTextColor: "#757575", // 4
+  inactiveField: "#F7F7F7", // 5
+  inactiveColor: "#767676", // 6
+  inactiveBG: "#e6e6e6", // 7
+  borderColor: "#DBDBDB", // 8
+  lightBorderColor: "#ededed", //9
+  primary: "#2957f5", // 10
+  primaryHover: "#869ef1", // 11
+}
+
+export default colors
